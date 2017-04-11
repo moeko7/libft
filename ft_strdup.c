3ff7fbd8911904a48ed8145ba9c9beaa41d40f56
Antoine PIECZYRAK <apieczyr@e1r4p12.42.fr>
2017-04-11 02:27:50 +0200

    atoi / strcmp / strdup / strlen / strncmp / .h

---
+++ ft_strdup.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apieczyr <apieczyr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/11 02:09:54 by apieczyr          #+#    #+#             */
+/*   Updated: 2017/04/11 02:26:11 by apieczyr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*strdup(const char *s1)
+{
+	unsigned int 	i;
+	char			*dest;
+	size_t			len;
+
+	len = ft_strlen(s1);
+	if (!s1)
+		return (NULL);
+	if (!(dest = (char *)malloc(sizeof(char) * len + 1)))
+		return (NULL);
+	while (i < len)
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
