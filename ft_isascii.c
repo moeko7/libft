8a01cba8b419c398b09fb062bf418e43483d7014
Antoine PIECZYRAK <apieczyr@e1r8p11.42.fr>
2017-04-16 21:23:12 +0200

    fixed includes

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,9 +6,11 @@
 /*   By: apieczyr <apieczyr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/12 20:41:56 by apieczyr          #+#    #+#             */
-/*   Updated: 2017/04/12 20:42:32 by apieczyr         ###   ########.fr       */
+/*   Updated: 2017/04/16 21:21:16 by apieczyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int		ft_isascii(int c)
 {
--- ft_putchar.c
+++ ft_putchar.c
@@ -6,11 +6,11 @@
 /*   By: apieczyr <apieczyr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/13 22:50:14 by apieczyr          #+#    #+#             */
-/*   Updated: 2017/04/13 22:50:41 by apieczyr         ###   ########.fr       */
+/*   Updated: 2017/04/16 21:22:02 by apieczyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "libft.h"
 
 void	ft_putchar(char c)
 {
